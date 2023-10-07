34aae70f0605b5d4f09b723cef415b1f682ea45f
kishwerjabeen <kishwerjabeen90@gmail.com>
2023-10-08 01:47:40 +0500

    index btn complated

--- pages/index.js
+++ pages/index.js
@@ -20,22 +20,53 @@ const Home = () => {
     <div className="bg-primary/60 h-full">
       {/* test */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto ">
+        <div className="text-center flex flex-col justify-center xl:pt-35 xl:text-left h-full container mx-auto ">
           {/* Title */}
-          <h1 className='h1'> Transforming Ideas <br /> Into
+          <motion.h1 
+          variants={fadeIn('down', 0.2)} 
+          initial='hidden'
+           animate='show' 
+           exit='hidden'
+          className='h1'> 
+          
+          Transforming Ideas <br /> Into
 
             <span className="text-accent"> Digital Reality </span>
 
-          </h1>
+          </motion.h1>
           {/* sub title */}
 
-          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
-          </p>
+          <motion.p
+
+            variants={fadeIn('down', 0.3)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">  
+            
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+
+          </motion.p>
 
           {/* btn */}
+
           <div className="flex justify-center xl:hidden relative ">
             <ProjectsBtn />
           </div>
+
+          <motion.div
+            variants={fadeIn('down', 0.4)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className="hidden xl:flex"
+
+          >
+
+            <ProjectsBtn />
+
+          </motion.div>
+
         </div>
       </div>
       {/* image */}
