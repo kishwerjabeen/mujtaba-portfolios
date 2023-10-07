6ee66161417d90d3e552cda1df40a04947472b73
kishwerjabeen <kishwerjabeen90@gmail.com>
2023-10-08 01:33:46 +0500

    project btn style

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -13,14 +13,15 @@ const ProjectsBtn = () => {
     <div className="mx-auto xl:mx-0">
 
       <Link href={'/work'} className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-no-repeat group p-4">
-        <Image 
-        src={'/rounded-text.png'}
-        width={135}
-        height={138}
-        alt=''
-        className="animate-spin-slow w-full h-full mx-w-[135px] mx-h-[138px]"
+        <Image
+          src={'/rounded-text.png'}
+          width={135}
+          height={138}
+          alt=''
+          className="animate-spin-slow w-full h-full mx-w-[135px] mx-h-[138px]"
         />
-<HiArrowRight className="absolute text-4xl group-hover:translate-x-2  transition-all duration-300" />
+        <HiArrowRight className="absolute text-4xl group-hover:translate-x-2  transition-all duration-300" />
+        
       </Link>
 
     </div>
