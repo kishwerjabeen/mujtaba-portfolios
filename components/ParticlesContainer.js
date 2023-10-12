9153612c58cad705a00df75838dd7c80e871ac67
kishwerjabeen <kishwerjabeen90@gmail.com>
2023-10-12 14:06:39 +0500

    start particles

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -1,5 +1,45 @@
+import { Particles } from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import React, { useCallback } from "react";
+
 const ParticlesContainer = () => {
-  return <div>Particles Container</div>;
+
+  // init
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+
+  }, [])
+
+
+  const particlesLoaded = useCallback(async (engine) => { }, []);
+
+  return
+  <Particles
+  className="w-full h-full absolute translate-z-0"
+    id="tsparticles"
+    init={particlesInit}
+    loaded={particlesLoaded}
+    options={
+      {
+        fullScreen: { enable: false },
+        background: {
+          color: {
+            value: '',
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: false,
+              mode: 'push',
+            }
+          }
+        }
+      }
+    }
+
+  />
 };
 
 export default ParticlesContainer;
