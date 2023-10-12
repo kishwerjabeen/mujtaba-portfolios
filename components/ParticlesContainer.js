61682b6883e81fa0132109b86fd17e906cd7ac8f
kishwerjabeen <kishwerjabeen90@gmail.com>
2023-10-12 15:33:55 +0500

    particales done

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -13,9 +13,9 @@ const ParticlesContainer = () => {
 
   const particlesLoaded = useCallback(async (engine) => { }, []);
 
-  return
+  return(
   <Particles
-  className="w-full h-full absolute translate-z-0"
+    className="w-full h-full absolute translate-z-0"
     id="tsparticles"
     init={particlesInit}
     loaded={particlesLoaded}
@@ -33,13 +33,71 @@ const ParticlesContainer = () => {
             onClick: {
               enable: false,
               mode: 'push',
+            },
+            onHover: {
+              enable: true,
+              mode: 'repulse',
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 90,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
             }
           }
-        }
+        },
+        particles: {
+          color: {
+            value: '#e68e2e'
+          },
+          links: {
+            color: '#f5d393',
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            outModes: {
+              default: 'bounce',
+            },
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number:{
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape:{
+            type: 'circle',
+          },
+          size:{
+            value: {min:1, max:5}, 
+          },
+        },
+        detectRetina: true,
       }
     }
 
+
   />
+  )
 };
 
 export default ParticlesContainer;
