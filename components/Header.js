cc43b45d1cae5469faddc1ba6176c09ba9bf37ae
kishwerjabeen <kishwerjabeen90@gmail.com>
2023-10-14 17:03:50 +0500

    Header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,35 @@
+//Next Image 
+import Image from "next/image";
+//Link
+import Link from "next/link";
+
+//components 
+import Social from "../components/Socials"
+
+
 const Header = () => {
   return (
-    <header className="absolute z-30 w-full">
-      Header
+    <header className="absolute z-30  w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+      <div className="container mx-auto ">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8"> 
+          {/* logo */}
+          <Link href={'/'}>
+            <Image
+              src={'/logo.svg'}
+              width={220}
+              height={48}
+              alt=""
+              priority={true}
+            >
+
+            </Image>
+          </Link>
+
+          {/* social */}
+          <Social />
+
+        </div>
+      </div>
     </header>
   );
 };
