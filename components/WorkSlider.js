61f6c73ec3c4c328cf69320ea2c61d9d4364dd3f
Kishwar Jabeen <kishwerjabeen90@gmail.com>
2023-10-24 05:56:20 +0500

    work on slide

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,3 +1,24 @@
+// imprt swiper Reat component 
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// import swper styles 
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+
+// icons
+import {
+  BsArrowRight
+} from "react-icons/bs";
+
+
+// imprt required module 
+import { Pagination } from 'swiper'
+
+//next image
+import Image from 'next/image';
+
 // data
 const workSlides = {
   slides: [
@@ -45,7 +66,45 @@ const workSlides = {
 };
 
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+  return (
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className='h-[280px] sm:h-[340px]'
+    >
+
+
+
+      {
+        workSlides.slides.map((Slide, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer' >
+                {Slide.images.map((images, index) => {
+                  return (
+                    <div className='relative rounded-lg overflow-hidden flex items-center justify-center group
+                    '>
+                      <div>
+                        {/* images */}
+                        <Image
+                          src={images.path}
+                          width={500}
+                          height={300}
+                          alt='' />
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+
+            </SwiperSlide>
+          );
+        })}
+    </Swiper >
+  );
 };
 
 export default WorkSlider;
